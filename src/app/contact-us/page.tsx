2a015b6a559da37f399c7b2625d917426b38a496
Juan Ramirez <jramirez203@outlook.com>
2025-07-15 01:55:04 -0500

    refactor(contact-us): convert to server component

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -161,12 +161,7 @@ const ContactForm = () => (
 
 export default function ContactUsPage() {
     return (
-        <div className="bg-white" style={{ fontFamily: "'Plus Jakarta Sans', 'Noto Sans', sans-serif" }}>
-            <style jsx global>{`
-              body {
-                background-image: url('data:image/svg+xml,%3Csvg width="60" height="60" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg"%3E%3Cg fill="none" fill-rule="evenodd"%3E%3Cg fill="%23edb5c4" fill-opacity="0.1"%3E%3Cpath d="M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E');
-              }
-            `}</style>
+        <div className="bg-white">
             <Header />
             <main className="flex-1 px-4 py-12 sm:px-6 lg:px-8">
                 <div
