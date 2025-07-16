9edbfd09b02d78e623c1354b08eca59dfbaf6399
Juan Ramirez <jramirez203@outlook.com>
2025-07-16 16:43:23 -0500

    fix(amplify): resolve build failure by defining product model

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,17 +1,15 @@
 import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 
 const schema = a.schema({
-  Todo: a
+  Product: a
     .model({
-      content: a.string(),
-      done: a.boolean(),
-      priority: a.enum(['low', 'medium', 'high']),
+      name: a.string().required(),
+      description: a.string(),
+      price: a.float().required(),
+      image: a.url(),
+      isFeatured: a.boolean().default(false),
     })
-    .authorization(
-      (allow) => [
-        allow.owner(),
-      ],
-      ),
+    .authorization((allow) => [allow.publicApiKey()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
