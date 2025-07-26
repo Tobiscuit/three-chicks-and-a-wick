b0af5e73dad17c3426fee74bbad40c91d0e18ea1
Juan Ramirez <jramirez203@outlook.com>
2025-07-26 14:00:15 -0500

    refactor(amplify): remove Product model to align with headless Shopify architecture

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,27 +1,18 @@
 import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 
-const schema = a.schema({
-  Product: a
-    .model({
-      name: a.string().required(),
-      description: a.string(),
-      price: a.float().required(),
-      image: a.url(),
-      isFeatured: a.boolean().default(false),
-    })
-    .authorization((allow) => [allow.publicApiKey()]),
-});
+// Schema is currently empty because products are sourced from Shopify.
+// We can add models here later for other features like user reviews.
+const schema = a.schema({});
 
 export type Schema = ClientSchema<typeof schema>;
 
 export const data = defineData({
   schema,
   authorizationModes: {
+    // This can be configured when we add models that require authorization.
     defaultAuthorizationMode: 'apiKey',
     apiKeyAuthorizationMode: {
       expiresInDays: 30,
     },
   },
-});
-
-// Force full rebuild 
+}); 
