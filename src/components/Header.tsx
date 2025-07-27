99a89951513a9d19346f46ec05568ff2d8d2b4ec
Juan Ramirez <jramirez203@outlook.com>
2025-07-27 18:21:28 -0500

    fix(components): await cookies in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { cookies } from 'next/headers';
 import HeaderClient from './HeaderClient';
 
 export default async function Header() {
-  const cookieStore = cookies();
+  const cookieStore = await cookies();
   const isLoggedIn = cookieStore.has('shopify_customer_token');
 
   return <HeaderClient isLoggedIn={isLoggedIn} />;
