8bf5454730299b79a0c6c855dfd289a85d126db8
Juan Ramirez <jramirez203@outlook.com>
2025-08-03 16:04:08 -0500

    test: deploy function only to isolate build error

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,9 +2,7 @@
 
 import { defineBackend } from '@aws-amplify/backend';
 import { magicRequest } from './functions/magic-request/resource';
-import { data } from './data/resource';
 
 export const backend = defineBackend({
   magicRequest,
-  data,
 });
