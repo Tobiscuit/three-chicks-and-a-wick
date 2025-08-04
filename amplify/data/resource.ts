02dec210318337f515a98a2f36d77253ad1ce962
Juan Ramirez <jramirez203@outlook.com>
2025-08-04 15:47:58 -0500

    Test model without authorization

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -3,7 +3,7 @@ import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 const schema = a.schema({
   Todo: a.model({
     content: a.string(),
-  }).authorization(allow => allow.publicApiKey()),
+  }),
 });
 
 export type Schema = ClientSchema<typeof schema>;
