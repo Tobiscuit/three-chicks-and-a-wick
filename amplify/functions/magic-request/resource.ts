8f80b38d0582502ebe32b50808fce3edad4cc3b8
Juan Ramirez <jramirez203@outlook.com>
2025-08-04 14:28:25 -0500

    Test function without environment variables to isolate CDK error

--- amplify/functions/magic-request/resource.ts
+++ amplify/functions/magic-request/resource.ts
@@ -1,11 +1,6 @@
-import { defineFunction, secret } from '@aws-amplify/backend';
+import { defineFunction } from '@aws-amplify/backend';
 
 export const magicRequestHandler = defineFunction({
   timeoutSeconds: 30,
   memoryMB: 512,
-  environment: {
-    GEMINI_API_KEY: secret('GEMINI_API_KEY'),
-    SHOPIFY_ADMIN_API_TOKEN: secret('SHOPIFY_ADMIN_API_TOKEN'),
-    SHOPIFY_STORE_DOMAIN: secret('SHOPIFY_STORE_DOMAIN'),
-  }
 });
