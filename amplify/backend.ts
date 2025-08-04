48954e81b44356399d6055a88b83e95f4c2c34d6
Juan Ramirez <jramirez203@outlook.com>
2025-08-04 12:51:47 -0500

    Test minimal schema alone - remove function from backend and simplify handler

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,6 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { data } from './data/resource';
-import { magicRequestHandler } from './functions/magic-request/resource';
 
 defineBackend({
-  data,
-  magicRequestHandler
+  data
 });
